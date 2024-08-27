b5e9f25d081c930684b23cff5840c3cb08fcdee4
Ben <kimaniben14@gmail.com>
2024-08-27 14:49:24 +0100

    still working on it

--- server/models.py
+++ server/models.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+# Define naming convention for foreign keys
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+# Initialize the database with the custom metadata
 db = SQLAlchemy(metadata=metadata)
 
 class Article(db.Model, SerializerMixin):
